bc747318f8957cf27cdbeb85e5d7350a209bebcb
MariceAshtiani <marice95@hotmail.no>
2022-12-27 17:09:15 +0100

    Adds templating for posts

---
+++ src/js/handlers/createPost.mjs
@@ -0,0 +1,20 @@
+import { createPost } from "../api/posts/index.mjs";
+
+export function setCreatePostFormListener() {
+    const form = document.querySelector("#createPost");
+
+    if (form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
+
+        const form = event.target;
+        const formData = new FormData(form);
+        const post = Object.fromEntries(formData.entries())
+        const action = form.action;
+        const method = form.method;
+    
+        // send it to the API
+        createPost(post)
+            })
+        }
+    }
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,24 +1,25 @@
 import * as constants from "./api/constants.mjs";
 
-import { setRegisterFormListener } from "./handlers/register.mjs";
-import { setLoginFormListener } from "./handlers/login.mjs";
-
+import * as listeners from "./handlers/index.mjs"
 import * as templates from "./templates/index.mjs"
 import * as postMethods from "./api/posts/index.mjs"
 
 const path = location.pathname;
 
 if (path === '/profile/login/' || path === '/profile/login/index.html') {
-    setLoginFormListener()
+    listeners.setLoginFormListener()
 } else if (path === '/profile/register/' || '/profile/register/index.html') {
-    setRegisterFormListener()
+    listeners.setRegisterFormListener()
+} else if (path === '/post/create/' || '/post/create/index.html') {
+    listeners.setCreatePostFormListener()
+} else if (path === '/post/edit/' || '/post/edit/index.html') {
+    listeners.setUpdatePostFormListener()
 }
 
 async function testTemplate() {
     const posts = await postMethods.getPosts();
-    const post = posts.pop()
-    const container = document.querySelector("#post");
-    templates.renderPostTemplate(post, container);
+    const container = document.querySelector("#posts");
+    templates.renderPostTemplates(posts, container);
 }
 
 testTemplate()
